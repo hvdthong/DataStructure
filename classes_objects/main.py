7f94144f6a800bcb582e1090dc383b4c33692e26
JHoangSMU <hvdthong@gmail.com>
2018-04-30 16:58:03 +0800

    adding classes

---
+++ classes_objects/main.py
@@ -0,0 +1,8 @@
+from robot import Robot
+
+if __name__ == "__main__":
+    robot1 = Robot("Tom", "red", 30)
+    robot2 = Robot("Jerry", "blue", 40)
+
+    robot1.introduce_self()
+    robot2.introduce_self()
---
+++ classes_objects/robot.py
@@ -0,0 +1,8 @@
+class Robot:
+    def __init__(self, name, color, weight):
+        self.name = name
+        self.color = color
+        self.weight = weight
+
+    def introduce_self(self):
+        print "My name is " + self.name

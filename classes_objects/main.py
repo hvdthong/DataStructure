d9cec41b362032ecb686a3bcc44145515fd4430b
JHoangSMU <hvdthong@gmail.com>
2018-08-05 17:12:40 +0800

    adding main

--- classes_objects/main.py
+++ classes_objects/main.py
@@ -3,22 +3,22 @@ from robot import Person
 from time_class import Time
 
 if __name__ == "__main__":
-    # robot1 = Robot("Tom", "red", 30)
-    # robot2 = Robot("Jerry", "blue", 40)
-    #
-    # robot1.introduce_self()
-    # robot2.introduce_self()
-    #
+    robot1 = Robot("Tom", "red", 30)
+    robot2 = Robot("Jerry", "blue", 40)
+
+    robot1.introduce_self()
+    robot2.introduce_self()
+
     # person1 = Person("Alice", "aggressive", False, robot1)
     # person1.robotOwned.introduce_self()
     #
     # time1 = Time(12, 30, 24)
     # time1.printTime()
-
-    robot1 = Robot()
-    robot1.name = "Tom"
-    robot1.color = "red"
-    robot1.weight = 30
-    robot1.introduce_self()
+    #
+    # robot1 = Robot()
+    # robot1.name = "Tom"
+    # robot1.color = "red"
+    # robot1.weight = 30
+    # robot1.introduce_self()
 
 
